8ae5f42526dda2600e66bbc8365f1fa5dd4fbb34
glaucio <glacsius@gmail.com>
2019-05-11 17:32:06 -0300

    tentativa de funcionar o flask

--- testemain.py
+++ testemain.py
@@ -1,2 +1,5 @@
 def retorno():
     return "Eu sou o glaucio ooooooo"
+
+if __name__ == "__main__":
+    print(retorno())
--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,21 @@
+
+# esse kra faz a ligação entre o apache e o python
+
+
 def application(environ, start_response):
     import sys
     path = '/var/www/webroot/ROOT'
     if path not in sys.path:
         sys.path.append(path)
-    from pyinfo import pyinfo
-    from testemain import retorno
-    output = pyinfo()
-    retorno = retorno()
+
+    # from pyinfo import pyinfo
+    # from testemain import retorno
+    from flask_simples import app as application
+
+    # output = pyinfo()
+    # retorno = retorno()
+
     start_response('200 OK', [('Content-type', 'text/html')])
-    #yield output.encode('utf-8')
-    yield  retorno.encode('utf-8')
+
+    # yield output.encode('utf-8')
+    # yield retorno.encode('utf-8')

cd37b7ade09bc76a0287a440f3c379bfdb33fb6d
glaucio <glacsius@gmail.com>
2019-05-13 08:48:29 -0300

    tentativa de funcionar o flask

--- flask_simples.py
+++ flask_simples.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask import request, render_template
 
-app = Flask(__name__)
+print("Menu nome:", __name__)
+print("Meu nome2:", __name__.split('.')[0])
+#app = Flask(__name__)
+app = Flask("qualquer")
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+# exemplo para responder essa url: http://localhost:8000/noticias/brasil?categoria=ciencia&quantidade=2
+@app.route("/noticias/<pais>")
+@app.route("/noticias/<pais>/<estado>")
+def lista_de_noticias(pais, estado=None):
+    cat = request.args.get("categoria")
+    qtd = request.args.get("quantidade")
+    #noticias = BD.query(pais=pais, categoria=cat).limit(qtd)
+    if not estado:
+        estado = 'nenhum'
+    noticias = 'Comedores de churros ' + cat + qtd + ' estado: ' + estado
+    #return render_template("lista_de_noticias.html", noticias=noticias), 200
+    return noticias
+
 
 if __name__ == "__main__":
     app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,27 @@
-
 # esse kra faz a ligação entre o apache e o python
 
+import sys
 
-def application(environ, start_response):
-    import sys
-    path = '/var/www/webroot/ROOT'
-    if path not in sys.path:
-        sys.path.append(path)
+sys.path.insert(0, "/var/www/webroot/ROOT")
+
+from flask_simples import app as application
+
+
+#def application(environ, start_response):
+#    import sys
+#    path = '/var/www/webroot/ROOT'
+#    if path not in sys.path:
+#        sys.path.append(path)
 
     # from pyinfo import pyinfo
     # from testemain import retorno
-    from flask_simples.hello import app as application
+    # from flask_simples import app as application
 
     # output = pyinfo()
     # retorno = retorno()
 
-    start_response('200 OK', [('Content-type', 'text/html')])
+    #start_response('200 OK', [('Content-type', 'text/html')])
 
     # yield output.encode('utf-8')
     # yield retorno.encode('utf-8')
-
-    yield 'churros ok'.encode('utf-8')
+    # yield 'churros ok'.encode('utf-8')
